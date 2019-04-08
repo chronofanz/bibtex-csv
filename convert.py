a2c6f6827b88f87861c5fd4f37b87c5a4dfacfce
Paul Chang (ECON) <pachang@microsoft.com>
2019-04-08 15:30:18 -0700

    Leverage pandas, fix utf8 encoding handling, and let the user choose how to merge similar fields (i.e. author, authors, editor -> author)

--- convert.py
+++ convert.py
@@ -8,64 +8,44 @@ Input is via standard input.
 Output is via standard output.
 """
 
-from os import getcwd
-from os import listdir
 from re import match
 from re import search
-from re import findall
-from sys import stdin
-from string import capwords
 
-entries = []
-entry = {}
+import pandas as pd
 
-for line in stdin:
-    if (match('^@', line.strip())):
-        if entry != {}:
-            entries.append(entry)
-            entry = {}
-    elif (match('url', line.strip())):
-        value, = findall('\{(\S+)\}', line)
-        entry["url"] = value
-    elif (search('=', line.strip())):
-        key, value = [v.strip(" {},\n") for v in line.split("=")]
-        entry[key] = value
-
-for entry in entries:
-    author = "Anonymous"
-    if "author" in entry:
-        author = entry["author"]
-    elif "authors" in entry:
-        author = entry["authors"]
-    elif "editor" in entry:
-        author = entry["editor"]
+''' Parses input file handle or text object holding bibtex formatted string data into a pandas dataframe. '''        
+def parse_bib(handle):
+    entries = []
+    entry = {}
     
-    title = "No title"
-    if "title" in entry:
-        title = entry["title"]
+    for line in handle:
+        if (match('^@', line.strip())):
+            if entry != {}:
+                entries.append(entry)
+                entry = {}
+        elif (match('url', line.strip())):
+            value, = findall('\{(\S+)\}', line)
+            entry["url"] = value
+        elif (search('=', line.strip())):
+            key, value = [v.strip(" {},\n") for v in line.split("=", 1)]
+            entry[key] = value
 
-    publish = "No publishing information"
-    if "journal" in entry:
-        publish = entry["journal"]
-    if "journaltitle" in entry:
-        publish = entry["journaltitle"]
-    elif "booktitle" in entry:
-        publish = entry["booktitle"]
-    elif "howpublished" in entry:
-        publish = entry["howpublished"]
-    elif "type" in entry:
-        publish = entry["type"]
-    elif "url" in entry:
-        publish = "Website: {}".format(entry["url"])
-    elif "crossref" in entry:
-        publish = entry["crossref"].replace("_", " ")
-        publish = capwords(publish)
-    elif "publisher" in entry:
-        publish = entry["publisher"]
+    keys = set()
+    for entry in entries:
+        for key in entry.keys():
+            keys.add(key)
+            
+    df = pd.DataFrame(columns=sorted(keys))
+    for entry in entries:
+        df = df.append(entry, ignore_index=True)
+                
+    return df
+
+def convert(input_file, output_file):
+    with open(input_file, 'r', encoding="utf8") as handle:
+        df = parse_bib(input_file)
+        df.to_csv(output_file, encoding='utf-8-sig')
     
-    year = "Unknown year"
-    if "year" in entry:
-        year = entry["year"]
-
-    print("{}\t{}\t{}\t{}".format(author, title, year, publish))
-
+if __name__ == "__main__":
+   convert(sys.argv[1], sys.argv[2])
+   
